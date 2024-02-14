79fa9fb220f956f0614b2b68e163c195d6193201
root <root@dockertest.localdomain>
2024-02-14 16:34:34 +0400

    Added travis file

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Change to Something to see the change !!
+          Change Something to Check the Change !!!
         </p>
         <a
           className="App-link"
